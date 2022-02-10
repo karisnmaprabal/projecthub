75f5801cd6c723c20f2595088e75d1bfe4040d27
karisnmaprabal <hinduppnepal@gmail.com>
2022-02-10 03:26:54 -0800

    'addeditems'

--- angularProj/src/app/app.module.ts
+++ angularProj/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Contact2Component } from './contact2/contact2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { Contact2Component } from './contact2/contact2.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
